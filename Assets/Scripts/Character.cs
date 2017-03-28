08075d4b6a08048bec61f8ae0883e08aefb4c71e
Hitsub <hitsumabushis@gmail.com>
2017-03-28 22:42:14 +0900

    走るモーション追加

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,9 +6,13 @@ public class Character : MonoBehaviour {
 	Vector3 moveDir;
 	Rigidbody rigidbody;
 
-	public float speed = 2f;
+	Vector3 walkAngle,runAngle;
+
+	public float walkSpeed = 2f;
+	public float runSpeed = 8f;
 
 	bool isWalk { get { return (Mathf.Abs (Input.GetAxis ("Vertical")) > 0.3f) || (Mathf.Abs (Input.GetAxis ("Horizontal")) > 0.3f); }}
+	bool isRun { get { return (Input.GetKey(KeyCode.LeftShift)); }}
 	bool isLeft { get { return (Input.GetAxis ("Horizontal") < -0.3f); }}
 	bool isRight { get { return (Input.GetAxis ("Horizontal") > 0.3f); }}
 	bool isForward { get { return (Input.GetAxis ("Vertical") > 0.3f); }}
@@ -17,20 +21,24 @@ public class Character : MonoBehaviour {
 	void Start () {
 		animator = this.GetComponent<Animator> ();
 		rigidbody = this.GetComponent<Rigidbody> ();
-		transform.eulerAngles = new Vector3 (0, 35, 0); //角度調整
+		walkAngle = new Vector3 (0, 35, 0); //角度調整
+		runAngle = new Vector3 (0, 0, 0); //角度調整
 	}
 
 	void Update () {
 		//AnimationControllerのパラメータ操作
 		animator.SetBool ("isWalk", isWalk);
+		animator.SetBool ("isRun", isRun);
 		animator.SetBool ("isLeft", isLeft);
 		animator.SetBool ("isRight", isRight);
 		animator.SetBool ("isForward", isForward);
 		animator.SetBool ("isBackward", isBackward);
 
+		transform.eulerAngles = (isRun) ? runAngle : walkAngle;
+
 		//キャラクタ移動
 		moveDir = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")).normalized;
-		rigidbody.velocity = moveDir * speed;
+		rigidbody.velocity = moveDir * ((isRun) ? runSpeed : walkSpeed);
 	}
 
 }
